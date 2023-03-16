9f177b2d2a7d0c1150db510218c5a3c08b097a77
saransh41 <119954310+saransh41@users.noreply.github.com>
2023-03-16 20:56:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,87 +1,128 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
-rover_height = 99;
-rover_width = 99;
+//Create an array "nasa_mars_images_array" and store all the image file names.
+nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg","nasa_image_3.jpg","nasa_image_4.jpg"]
 
+//Generate random number and store it in variable "random_number"
+random_number = Math.floor(Math.random() * 4);
 
-background_image = "mars.jpg";
+console.log(random_number);
+rover_width = 100;
+rover_height = 90;
+
+/*Pass this random_number as the index to the array and update the background_image variable 
+with the following*/
+background_image = nasa_mars_images_array[random_number];
+
+//console this background_image variable to see when images come, everytime we refresh the page.
+console.log("background_image = " + background_image);
+
 
 rover_image = "rover.png";
 
 rover_x = 10;
 rover_y = 10;
 
-//Create "add()" function
 function add() {
-    background_imgTag = new Image();
-    background_imgTag.onload = uploadBackground;
-    background_imgTag.src = background_image;
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
 
-    rover_imgTag = new Image();
-    rover_imgTag.onload = uploadrover;
-    rover_imgTag.src = rover_image;    
+	rover_imgTag = new Image(); //defining a variable with a new image
+	rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
+	rover_imgTag.src = rover_image;   // load image
 }
-//Create "uploadBackground()" function.
+
 function uploadBackground() {
-ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
 function uploadrover() {
-ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
 }
 
 
-
-
 window.addEventListener("keydown", my_keydown);
+
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-    
-	if(keyPressed == '38')
-    {
-        up();
-        console.log("up");
-    }	
-    if(keyPressed == '40')
-    {
-        down();
-        console.log("down");
-    }	
-    if(keyPressed == '37')
-    {
-        left();
-        console.log("left");
-    }	
-    if(keyPressed == '39')
-    {
-        right();
-        console.log("right");
-    }	
-     //Write code if keypressed is down. ASCII value of "up" arrow is 40.
-		
-
-
-
-
-    //Additional Activity
-    //Write the code for left and right arrow pressed. 
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
 }
 
-function up(){
-
+//Add the code for up() function:
+function up()
+{
+	if(rover_y >=0)
+	{
+		rover_y = rover_y - 10;
+		console.log("When up arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();
+	}
 }
-function down(){
+
+//Add the code for down() function:
+function down()
+{
+	if(rover_y <=500)
+	{
+		rover_y = rover_y + 10;
+		console.log("When down arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();	
+	}
+}
+
+//Add the code for right() function:
+function right()
+{
+	if(rover_x <=700)
+	{
+		rover_x = rover_x + 10;
+		console.log("When right arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();	
+	}
+}
+
+//Add the code for left() function:
+function left()
+{
+	if(rover_x >=0)
+	{
+		rover_x = rover_x - 10;
+		console.log("When left arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();	
+	}
+}
+
+
+
+
+
+
+
 	
-}
-function right(){
-	
-}
-function left(){
-	
-}
-
-
-
